5cf33f3b2e42658f42bc4b26fecca363dae8b57f
kinne <kinne@10.201.164.92>
2025-05-04 19:27:14 -0500

    (LIST) having it read and cleaning the words

--- Assignment7-2-ParkUNI/src/WordFrequencyAnalyzer.java
+++ Assignment7-2-ParkUNI/src/WordFrequencyAnalyzer.java
@@ -1,5 +1,19 @@
 
 public class WordFrequencyAnalyzer {
+	import java.io.*;
+	import java.nio.file.*;
+	import java.util.*;
 
+	public class WordFrequencyAnalyzer {
+	    public static void main(String[] args) throws IOException {
+	        List<String> words = new ArrayList<>();
+
+	        String content = Files.readString(Path.of("768008-0.txt"));
+	        content = content.replaceAll("[.,!?]", "").toLowerCase();
+	        String[] tokens = content.split("\\s+");
+
+	        words.addAll(Arrays.asList(tokens));
+	        System.out.println("Total words: " + words.size());
+	    }
+	}
 }
-test  
